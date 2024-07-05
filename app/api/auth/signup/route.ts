25a4a47c18912961e4f82261aeec5d1396280ac6
Daniel Fadamitan <b.fadamitan2019@gmail.com>
2024-07-05 02:01:07 +0100

    ⚙: changes route method

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -3,13 +3,14 @@ import { hash } from "bcryptjs";
 import { generateToken } from "@/lib/jwt";
 import { NextApiRequest, NextApiResponse } from "next";
 import { StudentType } from "@/types";
+import { NextRequest, NextResponse } from "next/server";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL || "",
   process.env.NEXT_PUBLIC_SUPABASE_KEY || ""
 );
 
-type Response = {
+type FetchResponse = {
   message: string;
   data?: {
     token: string;
@@ -18,10 +19,7 @@ type Response = {
   statusCode: number;
 };
 
-export async function POST(
-  req: NextApiRequest,
-  res: NextApiResponse<Response>
-) {
+export async function POST(req: NextRequest, res: NextResponse<FetchResponse>) {
   const {
     firstname,
     lastname,
@@ -34,7 +32,7 @@ export async function POST(
     password,
     sessionIn,
     sessionOut,
-  } = req.body;
+  } = await req.json();
 
   if (
     !firstname ||
@@ -49,9 +47,10 @@ export async function POST(
     !sessionIn ||
     !sessionOut
   ) {
-    return res
-      .status(400)
-      .json({ message: "All fields are required", statusCode: 400 });
+    return NextResponse.json(
+      { message: "All fields are required", statusCode: 400 },
+      { status: 400 }
+    );
   }
 
   try {
@@ -87,30 +86,36 @@ export async function POST(
 
     await supabase.from("students").update({ token }).eq("id", user?.id);
 
-    res.status(201).json({
-      data: {
-        token,
-        user: {
-          id: user.id,
-          firstname: user.firstname,
-          lastname: user.lastname,
-          email: user.email,
-          telephone: user.telephone,
-          gender: user.gender,
-          department: user.department,
-          faculty: user.faculty,
-          DOB: user.DOB,
-          session_in: user.session_in,
-          session_out: user.session_out,
-          verified: user.verified,
-          role: user.role,
+    NextResponse.json(
+      {
+        data: {
+          token,
+          user: {
+            id: user.id,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            email: user.email,
+            telephone: user.telephone,
+            gender: user.gender,
+            department: user.department,
+            faculty: user.faculty,
+            DOB: user.DOB,
+            session_in: user.session_in,
+            session_out: user.session_out,
+            verified: user.verified,
+            role: user.role,
+          },
         },
+        message: "User signed up successfully",
+        statusCode: 201,
       },
-      message: "User signed up successfully",
-      statusCode: 201,
-    });
+      { status: 201 }
+    );
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal server error", statusCode: 500 });
+    return NextResponse.json(
+      { message: "Internal server error", statusCode: 500 },
+      { status: 500 }
+    );
   }
 }
