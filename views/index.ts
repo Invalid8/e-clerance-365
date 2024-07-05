79f78d0c1cba43feacb73693ad53b3e686e7d9c9
Daniel Fadamitan <b.fadamitan2019@gmail.com>
2024-07-05 02:19:38 +0100

    ⚙: removed profile conmponet

--- views/index.ts
+++ views/index.ts
@@ -2,14 +2,12 @@ import Login from "./auth/login";
 import Register from "./auth/register";
 import Dashboard from "./dashboard";
 
-import Profile from "./dashboard/profile";
 import { NotEmail, NotNewsletter } from "./EmptyState/NotFound";
 import ErrorView from "./error";
 import NotFound from "./notFound";
 
 export {
   Dashboard,
-  Profile,
   Login,
   Register,
   ErrorView,
